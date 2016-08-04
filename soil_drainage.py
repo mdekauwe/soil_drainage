bab975de2c9a7b3ee96da397bb391a50b16a60e8
mdekauwe <mdekauwe@gmail.com>
2016-08-04 13:11:18 +1000

    first commit

---
+++ soil_drainage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Implement a simple multi-layer soil layer scheme to test I have implemented
+the Richards' equation correctly
+"""
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+__author__  = "Martin De Kauwe"
+__version__ = "1.0 (04.08.2016)"
+__email__   = "mdekauwe@gmail.com"
+
+
+def main():
+
+    soil_layers = np.zeros(10)
+
+
+
+if __name__ == "__main__":
+
+    main()
