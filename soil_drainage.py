9b9183b044dfd97ffd51a1930b2a28919381c75c
mdekauwe <mdekauwe@gmail.com>
2016-08-04 13:18:57 +1000

    Added soil layer thickness

--- soil_drainage.py
+++ soil_drainage.py
@@ -17,7 +17,8 @@ __email__   = "mdekauwe@gmail.com"
 
 def main():
 
-    soil_layers = np.zeros(10)
+    n_layers = 10
+    soil_layers = np.zeros(n_layers)
 
     # soil layer thickness in CABLE (m)
     zse = np.array([0.022, 0.058, 0.154, 0.409, 1.085, 2.872])
@@ -25,7 +26,7 @@ def main():
     # Use CABLE's sizing, but rescale over 2m as opposed to 4.6m
     proportions = zse / np.sum(zse)
     soil_layer_thickness = 2.0 * proportions
-    
+
 
 
     # fraction of root in each soil layer
