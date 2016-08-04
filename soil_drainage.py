94b8476cafba8903fa997a787e90f00768e2d80f
mdekauwe <mdekauwe@gmail.com>
2016-08-04 22:10:18 +1000

    Added transpiration extraction

--- soil_drainage.py
+++ soil_drainage.py
@@ -26,8 +26,7 @@ def layered_extraction_model(soil_layer_max, froot):
     n_days = 365
     n_layers = 6
 
-    # initialise layers to max
-    sw = soil_layer_max.copy()
+
 
     # generate some vaguely sensible rainfall inputs
     np.random.seed(0)
@@ -42,6 +41,19 @@ def layered_extraction_model(soil_layer_max, froot):
     canopy_evap = 0.0
     transpiration = 3.0 # mm d-1
 
+    # Coarse sand/Loamy sand
+    swilt = 0.072
+    sfc = 0.301
+    ssat = 0.398
+
+
+    # initialise layers to max
+    sw = np.zeros(n_layers)
+    for i in range(n_layers):
+        sw[i] = 0.9999 * ssat
+
+
+
     # just for plotting
     store = np.zeros((n_days, n_layers))
 
@@ -53,8 +65,31 @@ def layered_extraction_model(soil_layer_max, froot):
         drainage = 0.0
         extracted = 0.0
 
+        diff = 0.0
+        xxd = 0.0
         for j in range(n_layers):
 
+            xx = transpiration * froot[j] + diff
+            diff = max(0.0, sw[j] - swilt) * soil_layer_max[j]
+
+            xxd = xx - diff
+
+
+            if xxd > 0.0:
+
+                sw[j] -= diff / soil_layer_max[j]
+                diff = xxd
+
+            else:
+
+                sw[j] -= xx / soil_layer_max[j]
+                diff = xxd
+
+
+
+            """
+            print(xx, diff, sw[j]/soil_layer_max[j])
+            sys.exit()
             # if we need more water than we have available, only offer up what
             # the layer held
             if sw[j] + delta < 0.0:
@@ -72,6 +107,7 @@ def layered_extraction_model(soil_layer_max, froot):
                 extracted += delta
                 sw[j] += delta
                 delta = 0.0
+            """
 
             # store for plotting purposes
             store[i,j] = sw[j]
@@ -84,11 +120,11 @@ def layered_extraction_model(soil_layer_max, froot):
     count = 2
     for i in range(n_layers):
         ax = fig.add_subplot(n_layers+1,1,count)
-        ax.axhline(y=soil_layer_max[i], ls="--", color="k")
+        #ax.axhline(y=soil_layer_max[i], ls="--", color="k")
         ax.plot(store[:,i], color="royalblue", ls="-")
 
         ax.set_xlim(0, 365)
-        ax.set_ylim(0, soil_layer_max[i]+(soil_layer_max[i]*0.1))
+        #ax.set_ylim(0, soil_layer_max[i]+(soil_layer_max[i]*0.1))
         ax.set_ylabel("SW layer %d" % (i+1))
         if count < 6:
             plt.setp(ax.get_xticklabels(), visible=False)
